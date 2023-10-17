988a1182c7ee71044051ec66440050e4b719fe16
calvinpoh98 <calvinpoh01@gmail.com>
2023-10-17 11:39:25 +0800

    Added html

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const {engine} = require('express-handlebars');
 const PORT = parseInt(process.env.PORT) || 3000
 
 const app = express();
+app.use(express.static(__dirname + "/static"));
 
 app.listen(PORT,() => {
     console.info(`Application started on ${PORT} at ${new Date()}`);
