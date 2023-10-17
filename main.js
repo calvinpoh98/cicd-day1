83a70eacb266d4ddd083a0f4cee4d8a64112b3a4
calvinpoh98 <calvinpoh01@gmail.com>
2023-10-17 15:00:39 +0800

    additional features

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const {engine} = require('express-handlebars');
 const PORT = parseInt(process.env.PORT) || 3000
 
 const app = express();
+
+app.get('/time',(req,resp) => {
+    resp.send({time: (new Date()).toISOString()})
+})
+
 app.use(express.static(__dirname + "/static"));
 
 app.listen(PORT,() => {
